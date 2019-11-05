494b182648ed608e4f21adaedc8f93fefb6239cb
James Lucktaylor <jlucktay+github@gmail.com>
2019-11-05 15:20:10 +0000

    Retrieve single API by ID.

---
+++ tyk-dashboard-api/api-definitions/retrieve-a-single-api-by-id.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TYK_ROOT=$( dirname "${BASH_SOURCE[0]}" )/../..
+
+for lib in "${TYK_ROOT}"/lib/*.sh; do
+    # shellcheck disable=SC1090
+    source "$lib"
+done
+
+tyk::token
+
+curl \
+    --header "Authorization: ${token:-}" \
+    --request GET \
+    --silent \
+    "https://admin.cloudv2.tyk.io/api/apis/5dc04d2deafe810001a9a85b" \
+    | jq
